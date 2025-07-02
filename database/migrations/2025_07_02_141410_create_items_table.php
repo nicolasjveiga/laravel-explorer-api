8c901c94c4c5057d1826a91e30803e3ec55df11a
nicolasjveiga <nickjveiga@gmail.com>
2025-07-02 11:38:49 -0300

    ✨ Set up base models and migrations for explorers and item trading system

---
+++ database/migrations/2025_07_02_141410_create_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('value', 10, 2);
+            $table->decimal('latitude', 10, 7);
+            $table->decimal('longitude', 10, 7);
+            $table->foreignId('explorer_id')->constrained('explorers')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
